bfc7bb58b7ee7418aead09bf24c504d4bd3c5ec4
Kevin Falcone <kevin@edx.org>
2017-07-13 17:39:27 -0400

    more debugging

--- util/vpc-tools/abbey.py
+++ util/vpc-tools/abbey.py
@@ -535,7 +535,7 @@ extra_args_opts+=" -e@$extra_vars"
 
 env
 echo $0
-HOME=/root
+ANSIBLE_KEEP_REMOTE_FILES=1
 
 ansible-playbook -vvvv -c local -i "localhost," $play.yml $extra_args_opts
 ansible-playbook -vvvv -c local -i "localhost," stop_all_edx_services.yml $extra_args_opts
